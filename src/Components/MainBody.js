c93d97b348dc2a11789a113165f37e63b7d9f895
hasnainalig <117479412+hasnainalig@users.noreply.github.com>
2023-05-11 22:35:13 +0500

    new changes

--- src/Components/MainBody.js
+++ src/Components/MainBody.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Button, Input, Spin } from "antd";
-import config from "../../config";
+import config from "../config";
 
 const OPENAI_API_KEY = config.openai.apiKey; // secure -> environment variable
 const { TextArea } = Input;
